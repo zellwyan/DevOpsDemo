f712d173f7f772e37fa7dc7cf5d2790fb7ea8d63
zellwyan <zellwyan@students.zhaw.ch>
2023-06-02 10:28:12 +0200

    2 Tests Passed, 2 Failed

--- backend/src/main/java/ch/zhaw/iwi/devops/fizzbuzz/FizzBuzzConverter.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/fizzbuzz/FizzBuzzConverter.java
@@ -3,6 +3,12 @@ package ch.zhaw.iwi.devops.fizzbuzz;
 public class FizzBuzzConverter {
 
     public String convert(int i) {
+        if (i%6==0) {
+            return "Fizz";
+        } else if (i%8==0) {
+            return "Buzz";
+
+        }
        
     return String.valueOf (i);        
         }
